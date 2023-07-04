2070d133ec8d61c021496ff3ffb06a14a3850675
unknown <prodvictorkaio@gmail.com>
2023-07-04 09:55:47 -0300

    estruturação de pastas

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// Importar os módulos e pacotes necessários
+
+
+
+
+// Criar uma instaância do aplicativo Express
+
+
+// Middleware para análise do corpo das requisições como JSON
+
+// Middleware de autenticação global
+
+// Configurar as rotas
+
+// Rota inicial
+
+// Iniciar o servidor 
